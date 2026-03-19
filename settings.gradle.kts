de2454aee4396e7f96d647b06cd586f8fa796d80
shanfeng <shanfeng@uxin.com>
2026-03-19 16:10:17 +0800

    fix: prioritize Maven Central over Aliyun mirror in CI
    - Move google/mavenCentral to first position in settings.gradle.kts
    - CI will now use Maven Central directly instead of timing out on Aliyun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,24 +2,24 @@
 
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        google()
-        gradlePluginPortal()
-        mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
-        google()
-        mavenCentral()
     }
 }
 
