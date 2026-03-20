36c37da4ab8073d7d5e7afc57e0fd88e3e4ded2f
shanfeng <shanfeng@uxin.com>
2026-03-20 17:55:07 +0800

    fix: 为所有 business 模块添加 dokka 插件支持文档生成

--- business/data/build.gradle.kts
+++ business/data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.dokka)
 }
 
 android {
